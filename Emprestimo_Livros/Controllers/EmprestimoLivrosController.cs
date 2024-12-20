c3fabb2747408f8fadf617735d8ae1cf6f118a90
jpCaglianone <joao.caglianone@gmail.com>
2024-12-19 21:34:52 -0300

    Alteraçoes de nome de pagina e rota, alteraçoes na controller EmprestimoLivro e inclusao da pagina registro

--- Emprestimo_Livros/Controllers/EmprestimoLivrosController.cs
+++ Emprestimo_Livros/Controllers/EmprestimoLivrosController.cs
@@ -1,11 +1,14 @@
 ﻿using Emprestimo_Livros.Data;
 using Emprestimo_Livros.Models;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Emprestimo_Livros.Controllers
 {
+    [Route("EmprestimoLivros")]
     public class EmprestimoLivrosController : Controller
     {
+
 
         private readonly ApplicationDBContext _dbContext;
 
@@ -13,16 +16,38 @@ namespace Emprestimo_Livros.Controllers
         {
             _dbContext = dbContext;
         }
-
-        public IActionResult ListarEmprestimoLivros()
+        [HttpGet("Consultar")] //pode abstrair, mas to mantendo para deixar explicativo
+        public IActionResult Consultar()
         {
             IEnumerable<EmprestimoModel> model = _dbContext.EmprestimoLivros; 
-            return View("ListarEmprestimos", model);
+            return View("Consultar", model);
+        }
+        [HttpGet("Registrar")]
+        public IActionResult Registrar() //se o nome da classe nao tiver de acordo com a asp-controller,
+                                         //nao funciona, se o nome da classe for igual ao da view, nao precisa
+                                         //colocar o nome da view, pode deixar return View() 
+        {
+            ViewData["Title"] = "Registrar novo empréstimo";
+            DateTime data = DateTime.Now;
+            ViewBag.data = data;
+            return View("Registrar");
         }
 
-        public IActionResult RegistrarEmprestimoLivro()
+        [HttpPost("Registrar")] //no form, o nome Registrar precisa estar no 
+        public IActionResult ResultadoRegistrar(EmprestimoModel emprestimo) 
         {
-            return View("RegistrarEmprestimos");
+            bool sucesso = false;
+            ViewBag.sucesso = sucesso;
+            if (!sucesso)
+            {
+                ViewBag.mensagem = "Não foi possivel realizar o registro!";
+            }
+            else
+            {
+                ViewBag.mensagem = "Registro realizado com sucesso!";
+            }
+                
+            return View("Registrar");
         }
     }
 }
