b8361c6c3e65bc509093209722a878d24baf454d
jpCaglianone <joao.caglianone@gmail.com>
2024-12-20 01:05:00 -0300

    Alteraçao na controller de emprestimo de livros e na view registro de livros

--- Emprestimo_Livros/Controllers/EmprestimoLivrosController.cs
+++ Emprestimo_Livros/Controllers/EmprestimoLivrosController.cs
@@ -1,53 +1,63 @@
 ﻿using Emprestimo_Livros.Data;
 using Emprestimo_Livros.Models;
 using Microsoft.AspNetCore.Mvc;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Emprestimo_Livros.Controllers
 {
     [Route("EmprestimoLivros")]
     public class EmprestimoLivrosController : Controller
     {
-
-
         private readonly ApplicationDBContext _dbContext;
 
         public EmprestimoLivrosController(ApplicationDBContext dbContext)
         {
             _dbContext = dbContext;
         }
-        [HttpGet("Consultar")] //pode abstrair, mas to mantendo para deixar explicativo
+
+        [HttpGet("Consultar")]
         public IActionResult Consultar()
         {
-            IEnumerable<EmprestimoModel> model = _dbContext.EmprestimoLivros; 
+            IEnumerable<EmprestimoModel> model = _dbContext.EmprestimoLivros;
             return View("Consultar", model);
         }
+
         [HttpGet("Registrar")]
-        public IActionResult Registrar() //se o nome da classe nao tiver de acordo com a asp-controller,
-                                         //nao funciona, se o nome da classe for igual ao da view, nao precisa
-                                         //colocar o nome da view, pode deixar return View() 
+        public IActionResult Registrar()
         {
-            ViewData["Title"] = "Registrar novo empréstimo";
-            DateTime data = DateTime.Now;
-            ViewBag.data = data;
-            return View("Registrar");
+            ViewBag.sucesso = null;
+            var model = new EmprestimoModel
+            {
+                DataEmprestimoLivro = DateTime.Now 
+            };
+            return View("Registrar", model);
         }
 
-        [HttpPost("Registrar")] //no form, o nome Registrar precisa estar no 
-        public IActionResult ResultadoRegistrar(EmprestimoModel emprestimo) 
+        [HttpGet("api/Consultar")]
+        public IEnumerable<EmprestimoModel> ApiConsultar()
         {
-            bool sucesso = false;
-            ViewBag.sucesso = sucesso;
-            if (!sucesso)
+            IEnumerable<EmprestimoModel> lista = _dbContext.EmprestimoLivros.ToList();
+            return lista;
+        }
+
+        [HttpPost("Registrar")]
+        public IActionResult ResultadoRegistrar(EmprestimoModel emprestimo)
+        {
+            emprestimo.DataEmprestimoLivro = DateTime.Now;
+
+            if (ModelState.IsValid)
             {
-                ViewBag.mensagem = "Não foi possivel realizar o registro!";
+                _dbContext.EmprestimoLivros.Add(emprestimo);
+                _dbContext.SaveChanges();
+                ViewBag.sucesso = true;
+                ViewBag.mensagem = "Registro realizado com sucesso!";
             }
             else
             {
-                ViewBag.mensagem = "Registro realizado com sucesso!";
+                ViewBag.sucesso = false;
+                ViewBag.mensagem = "Não foi possível realizar o registro!";
             }
-                
-            return View("Registrar");
+
+            return View("Registrar", emprestimo);
         }
     }
 }
